0e866cada1c44aa214d6ab791fcc372264a2f246
Aditto <avikd906@proton.me>
2024-11-26 23:22:42 +0530

    added aos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,25 @@
-import Banner from "../components/Banner"
-import About from "./About"
+import { useEffect } from "react";
+import Banner from "../components/Banner";
+import About from "./About";
+import AOS from "aos"; // Import AOS
+import "aos/dist/aos.css"; // Import AOS styles
 
 function Home() {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Duration of animation
+            once: true, // Whether the animation should happen only once
+        });
+    }, []);
+
     return (
         <div>
-            <Banner></Banner>
+            <Banner data-aos="fade-up"></Banner> {/* Apply AOS animation */}
             <div className="mt-10">
-                <About></About>
+                <About data-aos="fade-left"></About> {/* Apply AOS animation */}
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
